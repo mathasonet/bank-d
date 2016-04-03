0ed83758b79c57c4047cd862065c98a6604f1b6b
Mat Haverty <mathasonet@me.com>
2016-04-02 22:42:03 -0700

    Initial Commit

---
+++ Bank'd/VC.swift
@@ -0,0 +1,24 @@
+//
+//  VC.swift
+//  Bank'd
+//
+//  Created by Mathew Haverty on 4/2/16.
+//  Copyright © 2016 Matify. All rights reserved.
+//
+
+import UIKit
+
+class VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
